5d42ab88d388c69d5dc4a3cbb3b3cb8afaa9e990
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 23:51:31 +1300

    make middleware for authenticated route

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,9 +1,27 @@
-import { initTRPC } from '@trpc/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/server'
+import { TRPCError, initTRPC } from '@trpc/server'
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
 const t = initTRPC.create()
+const middleware = t.middleware
+const isAuth = middleware(async (opts) => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  return opts.next({
+    ctx: {
+      userId: user.id,
+      user,
+    },
+  })
+})
 // Base router and procedure helpers
 export const router = t.router
 export const publicProcedure = t.procedure
+export const privateProcedure = t.procedure.use(isAuth)
