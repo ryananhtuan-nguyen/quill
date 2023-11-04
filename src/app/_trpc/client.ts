bad2831ddb1ec8583ca043cc198ee5a6e77590da
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 21:05:27 +1300

    change source path

---
+++ src/app/_trpc/client.ts
@@ -0,0 +1,3 @@
+import { CreateTRPCReact, createTRPCReact } from '@trpc/react-query'
+
+export const trpc = createTRPCReact<>({})
---
+++ src/trpc/index.ts
@@ -0,0 +1,7 @@
+import { router } from './trpc'
+const appRouter = router({
+  // ...
+})
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter
