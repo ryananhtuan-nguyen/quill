b61ea3fb27dcdcd740ad3439bb2b20633ac9d582
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 21:46:09 +1300

    public procedures

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -6,4 +6,4 @@ import { initTRPC } from '@trpc/server'
 const t = initTRPC.create()
 // Base router and procedure helpers
 export const router = t.router
-export const procedure = t.procedure
+export const publicProcedure = t.procedure
