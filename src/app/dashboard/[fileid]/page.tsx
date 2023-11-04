2771af5d4a06a7a80f3f4fa05b536eea610f94dc
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-05 12:53:29 +1300

    chatwrapper and pdfrender wrapper

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,48 @@
+import ChatWrapper from '@/components/ChatWrapper'
+import PdfRenderer from '@/components/PdfRenderer'
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/server'
+import { notFound, redirect } from 'next/navigation'
+
+interface PageProps {
+  params: {
+    fileid: string
+  }
+}
+
+const Page = async ({ params }: PageProps) => {
+  //retrieve the file id
+  const { fileid } = params
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`)
+  //make databse call
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  })
+
+  if (!file) notFound()
+
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[cal(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* left side / pdf view */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer />
+          </div>
+        </div>
+
+        {/* right side / chat box */}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page
