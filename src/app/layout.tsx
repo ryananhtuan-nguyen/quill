eef16fb333bee8002180b50f1896417c311d04e6
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 22:09:56 +1300

    wrap with Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/libs/utils'
 import NavBar from '@/components/NavBar'
+import Providers from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,15 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
-      <body
-        className={cn(
-          'min-h-screen font-sans antialiased grainy',
-          inter.className
-        )}
-      >
-        <NavBar />
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}
+        >
+          <NavBar />
+          {children}
+        </body>
+      </Providers>
     </html>
   )
 }
