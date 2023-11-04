bf7c345d30487e258ee4ca2d92274221fff0c35c
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-04 18:16:30 +1300

    done with wrapper + clsx + tailwindmerge

--- src/component/MaxWidthWrapper.tsx
+++ src/component/MaxWidthWrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/libs/utils'
 import React from 'react'
 
 const MaxWidthWrapper = ({
@@ -8,7 +9,12 @@ const MaxWidthWrapper = ({
   children: React.ReactNode
 }) => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+    <div
+      className={cn(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}
+    >
       {children}
     </div>
   )
