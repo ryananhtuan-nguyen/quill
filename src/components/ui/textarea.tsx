da2a594c2c31491920d1828d300834f98b7deea7
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-05 19:24:30 +1300

    custom maxRows

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,16 +1,18 @@
-import * as React from "react"
-
-import { cn } from "@/libs/utils"
+import * as React from 'react'
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from 'react-textarea-autosize'
+import { cn } from '@/libs/utils'
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
   ({ className, ...props }, ref) => {
     return (
-      <textarea
+      <TextareaAutosize
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          'flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
@@ -19,6 +21,6 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     )
   }
 )
-Textarea.displayName = "Textarea"
+Textarea.displayName = 'Textarea'
 
 export { Textarea }
