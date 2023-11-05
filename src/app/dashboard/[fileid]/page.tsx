f96bde94a9eb24ef9b671cdf2005a08521db9796
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-05 19:06:50 +1300

    file id props

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({ params }: PageProps) => {
 
         {/* right side / chat box */}
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
