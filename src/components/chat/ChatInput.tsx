80fff91695e94ff6da818495de01f992e498a56f
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-06 12:33:50 +1300

    switching form to normal div

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -16,7 +16,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
@@ -40,7 +40,6 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
 
               <Button
                 disabled={isLoading || isDisabled}
-                type="submit"
                 onClick={() => {
                   addMessage()
 
@@ -54,7 +53,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
