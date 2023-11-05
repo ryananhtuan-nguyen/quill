cf29a7e97efcb93c94ef3af6ce5068d9c086f290
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-05 14:38:03 +1300

    on upload success

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,29 +1,30 @@
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/server'
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 
 const f = createUploadthing()
 
-const auth = (req: Request) => ({ id: 'fakeId' }) // Fake auth function
-
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: '4MB' } })
-    // Set permissions and file types for this FileRoute
+  pdfUploader: f({ image: { maxFileSize: '4MB' } })
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req)
+      const { getUser } = getKindeServerSession()
+      const user = await getUser()
 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new Error('Unauthorized')
+      if (!user || !user.id) throw new Error('UNAUTHORIZED')
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log('Upload complete for userId:', metadata.userId)
-
-      console.log('file url', file.url)
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: 'PROCESSING',
+        },
+      })
     }),
 } satisfies FileRouter
 
