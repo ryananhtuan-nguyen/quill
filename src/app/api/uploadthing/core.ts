e1187de9bb3722f67e339bc7a8dcf46527ec6dd7
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-05 14:58:43 +1300

    configured api upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,7 +6,7 @@ const f = createUploadthing()
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  pdfUploader: f({ image: { maxFileSize: '4MB' } })
+  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession()
       const user = await getUser()
