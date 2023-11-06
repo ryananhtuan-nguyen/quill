0c4c8568c994af43563c0620703917c3063a5a71
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-06 21:26:21 +1300

    metadata for title description & search engine

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,4 +1,5 @@
 import clsx, { ClassValue } from 'clsx'
+import { Metadata } from 'next'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,4 +10,48 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
   return `http://localhost:${process.env.PORT ?? 3000}${path}`
+}
+
+export function constructMetadata({
+  title = 'Quill - the Saas for students',
+  description = 'Quill is an open-source software to make chatting to your PDF files easy.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@ryan',
+    },
+    icons,
+    metadataBase: new URL('https://quill-sooty-zeta.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
