8283f87c7ec7bd15aa99299bd589574dadaccfea
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-06 18:11:59 +1300

    fixing deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,11 +6,11 @@ import { redirect } from 'next/navigation'
 const Page = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
-  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  if (!user || !user?.id) redirect('/auth-callback?origin=dashboard')
 
   const dbUser = await db.user.findFirst({
     where: {
-      id: user.id,
+      id: user?.id,
     },
   })
 
