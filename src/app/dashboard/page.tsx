b3b9608f6f09b9b4c4700846ae300b6e09779182
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-06 22:12:12 +1300

    fixing callback

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import Dashboard from '@/components/Dashboard'
 import { db } from '@/db'
 import { getUserSubscriptionPlan } from '@/libs/stripe'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
 
 const Page = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
+
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
   const dbUser = await db.user.findFirst({
