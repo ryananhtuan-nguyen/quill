5766b9b3da1cadb13aa53c215b916ac2e64b8be5
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-06 21:59:25 +1300

    implement pro and free plan

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,74 +5,120 @@ import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { pinecone } from '@/libs/pinecone'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { getUserSubscriptionPlan } from '@/libs/stripe'
+import { PLANS } from '@/config/stripe'
 
 const f = createUploadthing()
 
-// FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
-    .middleware(async ({ req }) => {
-      const { getUser } = getKindeServerSession()
-      const user = await getUser()
+const middleware = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-      if (!user || !user.id) throw new Error('UNAUTHORIZED')
+  if (!user || !user.id) throw new Error('UNAUTHORIZED')
 
-      return { userId: user.id }
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      const createdFile = await db.file.create({
+  const subscriptionPlan = await getUserSubscriptionPlan()
+
+  return { subscriptionPlan, userId: user.id }
+}
+
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>
+  file: {
+    key: string
+    name: string
+    url: string
+  }
+}) => {
+  const isFileExist = await db.file.findFirst({
+    where: {
+      key: file.key,
+    },
+  })
+
+  if (isFileExist) return
+
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: 'PROCESSING',
+    },
+  })
+  try {
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    )
+    const blob = await response.blob()
+
+    const loader = new PDFLoader(blob)
+
+    const pageLevelDocs = await loader.load()
+
+    const pagesAmt = pageLevelDocs.length
+
+    const { subscriptionPlan } = metadata
+    const { isSubscribed } = subscriptionPlan
+
+    const isProExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
+    const isFreeExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === 'Free')!.pagesPerPdf
+
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+      await db.file.update({
         data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: 'PROCESSING',
+          uploadStatus: 'FAILED',
+        },
+        where: {
+          id: createdFile.id,
         },
       })
-      try {
-        const response = await fetch(
-          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-        )
-        const blob = await response.blob()
+    }
+    //vectorize and index entire document
 
-        const loader = new PDFLoader(blob)
+    const pineconeIndex = pinecone.Index('quill')
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    })
 
-        const pageLevelDocs = await loader.load()
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      // namespace: createdFile.id,
+    })
 
-        const pagesAmt = pageLevelDocs.length
-
-        //vectorize and index entire document
-
-        const pineconeIndex = pinecone.Index('quill')
-        const embeddings = new OpenAIEmbeddings({
-          openAIApiKey: process.env.OPENAI_API_KEY,
-        })
-
-        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-          pineconeIndex,
-          // namespace: createdFile.id,
-        })
-
-        await db.file.update({
-          data: {
-            uploadStatus: 'SUCCESS',
-          },
-          where: {
-            id: createdFile.id,
-          },
-        })
-      } catch (err) {
-        console.log(err)
-        await db.file.update({
-          data: {
-            uploadStatus: 'FAILED',
-          },
-          where: {
-            id: createdFile.id,
-          },
-        })
-      }
-    }),
+    await db.file.update({
+      data: {
+        uploadStatus: 'SUCCESS',
+      },
+      where: {
+        id: createdFile.id,
+      },
+    })
+  } catch (err) {
+    console.log(err)
+    await db.file.update({
+      data: {
+        uploadStatus: 'FAILED',
+      },
+      where: {
+        id: createdFile.id,
+      },
+    })
+  }
+}
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
