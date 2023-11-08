4aa1217e8bbe4d1c35ea4953319c562d8e985eb5
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-08 16:56:51 +1300

    MVP

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { trpc } from '@/app/_trpc/client'
 import UploadButton from './UploadButton'
 import { Ghost, Loader2, MessageSquare, Plus, Trash } from 'lucide-react'
@@ -17,8 +18,11 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
   const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
     string | null
   >(null)
-  const utils = trpc.useUtils()
+
+  const utils = trpc.useContext()
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery()
+
   const { mutate: deleteFile } = trpc.deleteFile.useMutation({
     onSuccess: () => {
       utils.getUserFiles.invalidate()
@@ -31,19 +35,17 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
     },
   })
 
-  console.log(files)
-
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-        <h1 className="mb-3 font-bold text-5xl text-gray-900">My files</h1>
+        <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
 
         <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
       </div>
 
       {/* display all user files */}
       {files && files?.length !== 0 ? (
-        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grids-cols-3">
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
           {files
             .sort(
               (a, b) =>
@@ -76,21 +78,22 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
                     <Plus className="h-4 w-4" />
                     {format(new Date(file.createdAt), 'MMM yyyy')}
                   </div>
+
                   <div className="flex items-center gap-2">
                     <MessageSquare className="h-4 w-4" />
-                    {file.name}
+                    mocked
                   </div>
 
                   <Button
+                    onClick={() => deleteFile({ id: file.id })}
                     size="sm"
                     className="w-full"
                     variant="destructive"
-                    onClick={() => deleteFile({ id: file.id })}
                   >
                     {currentlyDeletingFile === file.id ? (
                       <Loader2 className="h-4 w-4 animate-spin" />
                     ) : (
-                      <Trash className="my-2" />
+                      <Trash className="h-4 w-4" />
                     )}
                   </Button>
                 </div>
@@ -103,7 +106,7 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
         <div className="mt-16 flex flex-col items-center gap-2">
           <Ghost className="h-8 w-8 text-zinc-800" />
           <h3 className="font-semibold text-xl">Pretty empty around here</h3>
-          <p>Let&apos;s upload your first PDF</p>
+          <p>Let&apos;s upload your first PDF.</p>
         </div>
       )}
     </main>
