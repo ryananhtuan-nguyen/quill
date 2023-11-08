5ab6b05cfc2871e6f6a350246179062b536cfb1d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-08 16:57:33 +1300

    MVP

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -12,7 +12,7 @@ import Image from 'next/image'
 import { Icons } from './Icons'
 import Link from 'next/link'
 import { Gem } from 'lucide-react'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/dist/components'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
 
 interface UserAccountNavProps {
   email: string | undefined
@@ -50,10 +50,11 @@ const UserAccountNav = async ({
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent className="bg-white" align="end">
-        <div className="flex items-center jusitfy-start gap-2 p-2">
+        <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-0.5 leading-none">
-            {name && <p className="font-medium text-sm text-black"></p>}
+            {name && <p className="font-medium text-sm text-black">{name}</p>}
             {email && (
               <p className="w-[200px] truncate text-xs text-zinc-700">
                 {email}
@@ -61,10 +62,13 @@ const UserAccountNav = async ({
             )}
           </div>
         </div>
+
         <DropdownMenuSeparator />
+
         <DropdownMenuItem asChild>
           <Link href="/dashboard">Dashboard</Link>
         </DropdownMenuItem>
+
         <DropdownMenuItem asChild>
           {subscriptionPlan?.isSubscribed ? (
             <Link href="/dashboard/billing">Manage Subscription</Link>
@@ -76,6 +80,7 @@ const UserAccountNav = async ({
         </DropdownMenuItem>
 
         <DropdownMenuSeparator />
+
         <DropdownMenuItem className="cursor-pointer">
           <LogoutLink>Log out</LogoutLink>
         </DropdownMenuItem>
